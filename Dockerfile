ec1f2db2870f7f0136f4acf9cebdf630c5be72f2
dohlee <d0013159@gmail.com>
2021-04-20 20:08:15 +0900

    t1

--- Dockerfile
+++ Dockerfile
@@ -4,35 +4,22 @@ LABEL   maintainer = "dohlee@student.42seoul.kr"
 
 RUN     apt-get update && apt-get install -y \
         nginx \
-        mariadb-server \
-        mariadb-client \
         php-mysql \
         php-mysql \
         php-mbstring \
         openssl \
         vim \
         php7.3-fpm \
-        wget
+        wget \
+		mariadb-server
 
 RUN wget https://wordpress.org/latest.tar.gz
-RUN tar -xvf latest.tar.gz && mv wordpress var/www/html/
-
-RUN chown -R www-data:www-data /var/www/html/
-RUN chmod -R 755 /var/www/html/
-
 RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
-RUN	tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
-RUN mv phpMyAdmin-5.0.2-all-languages phpmyadmin && mv phpmyadmin /var/www/html/
-
-
-RUN rm phpMyAdmin-5.0.2-all-languages.tar.gz && rm latest.tar.gz
 
 ADD ./srcs/default /etc/nginx/sites-available/default
-ADD ./srcs/wp-config.php /var/www/html/wordpress
-ADD ./srcs/config.inc.php /var/www/html/phpmyadmin
-ADD ./srcs/run.sh ./
-ADD ./srcs/dohlee+3.pem etc/ssl/certs/dohlee+3.pem
-ADD ./srcs/dohlee+3-key.pem etc/ssl/private/dohlee+3-key.pem
+ADD ./srcs/wp-config.php ./tmp/wp-config.php
+ADD ./srcs/config.inc.php ./tmp/config.inc.php
+ADD ./srcs/run.sh /
 
 EXPOSE 80 443
 
--- srcs/run.sh
+++ srcs/run.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-chmod 775 /run.sh
 
-chown -R mysql:mysql /var/run/mysqld/mysqld.sock
-chown -R mysql:mysql /var/run/mysqld/mysqld.sock
-chmod -R 644 /var/run/mysqld/mysqld.sock
 
-/etc/init.d/mysql start
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
+mv localhost.dev.crt etc/ssl/certs/ && mv localhost.dev.key etc/ssl/private/
+chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
+
+service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" \
 	| mysql -u root --skip-password
 echo "CREATE USER IF NOT EXISTS 'dohlee'@'localhost' IDENTIFIED BY 'dohlee';" \
 	| mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'dohlee'@'localhost' WITH GRANT OPTION;" \
 	| mysql -u root --skip-password
+chown -R www-data:www-data /var/www/html/wordpress
+chmod -R 755 /var/www/html
+tar -xzf latest.tar.gz && mv  wordpress var/www/html/
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 
-service nginx start && service php7.3-fpm start
+mv phpMyAdmin-5.0.2-all-languages phpmyadmin && mv phpmyadmin /var/www/html/
+
+rm phpMyAdmin-5.0.2-all-languages.tar.gz && rm latest.tar.gz
+cp -rp /tmp/wp-config.php /var/www/html/wordpress && cp -rp /tmp/config.inc.php var/www/html/phpmyadmin
+
+service nginx start
+service php7.3-fpm start
 service mysql restart
 
 bash
