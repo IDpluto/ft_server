234ab443aa0c3f28ff4365402cfabd195d7ff265
dohlee <d0013159@gmail.com>
2021-04-23 16:50:42 +0900

    rfin

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM    debian:buster
 
 LABEL   maintainer = "dohlee@student.42seoul.kr"
 
+ENV AUTOINDEX on
+
 RUN	apt-get update && apt-get install -y \
     nginx \
     mariadb-server \
@@ -13,11 +15,11 @@ RUN	apt-get update && apt-get install -y \
     php7.3-fpm
 
 COPY ./srcs/run.sh ./
-COPY ./srcs/default ./tmp
 COPY ./srcs/wp-config.php ./tmp
 COPY ./srcs/config.inc.php ./tmp
+COPY ./srcs/index_on ./tmp
+COPY ./srcs/index_off ./tmp
 
 EXPOSE 80 443
 
 CMD bash run.sh
-
--- srcs/run.sh
+++ srcs/run.sh
@@ -7,13 +7,10 @@ chmod -R 755 /var/www/
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
 mv localhost.dev.crt etc/ssl/certs/ && mv localhost.dev.key etc/ssl/private/
 chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
-
-cp -rp /tmp/default /etc/nginx/sites-available
-
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz && mv wordpress var/www/html/
 chown -R www-data:www-data /var/www/html/wordpress
-cp -rp ./tmp/wp-config.php /var/www/html/wordpress
+cp -rp /tmp/wp-config.php /var/www/html/wordpress
 
 service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" \
@@ -26,10 +23,16 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'dohlee'@'localhost' WITH GRANT OPT
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin && mv phpmyadmin /var/www/html/
-cp -rp ./tmp/config.inc.php var/www/html/phpmyadmin/
+cp -rp /tmp/config.inc.php var/www/html/phpmyadmin/
 
 rm phpMyAdmin-5.0.2-all-languages.tar.gz && rm latest.tar.gz
 
+if [ "$AUTOINDEX" = "off" ] ;
+then cp /tmp/index_off /etc/nginx/sites-available/default ;
+else cp /tmp/index_on /etc/nginx/sites-available/default ;
+fi
+
+rm /tmp/*
 service nginx start
 service php7.3-fpm start
 service mysql restart
